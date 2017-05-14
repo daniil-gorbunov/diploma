eaa8f1467f724ed026b31f7dc14281993b1b6307
Daniil <godastan@gmail.com>
2017-05-14 23:52:40 +0300

    mock

--- src/app/order-creator/order-creator.component.ts
+++ src/app/order-creator/order-creator.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
-  OnInit
+  Input,
+  OnInit,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './order-creator.component.html',
 })
 export class OrderCreatorComponent implements OnInit {
+  @Input() public cafes: any[];
 
   public localState: any;
   constructor(
@@ -26,22 +28,16 @@ export class OrderCreatorComponent implements OnInit {
       });
 
     console.log('hello `About` component');
-    // static data that is bundled
-    // var mockData = require('assets/mock-data/mock-data.json');
-    // console.log('mockData', mockData);
-    // if you're working with mock data you can also use http.get('assets/mock-data/mock-data.json')
     this.asyncDataWithWebpack();
   }
+
   private asyncDataWithWebpack() {
-    // you can also async load mock data with 'es6-promise-loader'
-    // you would do this if you don't want the mock-data bundled
-    // remember that 'es6-promise-loader' is a promise
     setTimeout(() => {
 
-      System.import('../../assets/mock-data/mock-data.json')
-        .then((json) => {
-          console.log('async mockData', json);
-          this.localState = json;
+      System.import('../../assets/mock-data/mock-cafe.json')
+        .then((cafes) => {
+          console.log('async mockData', cafes);
+          this.cafes = cafes;
         });
 
     });
